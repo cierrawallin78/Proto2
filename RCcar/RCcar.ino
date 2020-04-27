089cda56706984ac1eaa247babd9d158563eb2d9
cierrawallin78 <54870269+cierrawallin78@users.noreply.github.com>
2020-04-26 23:03:07 -0600

    Update RCcar.ino

--- RCcar/RCcar.ino
+++ RCcar/RCcar.ino
@@ -1,15 +1,15 @@
 
-#define ENA_m1 10        // Enable/speed motor Front Right Left
-#define ENB_m1 3        // Enable/speed motor Back Right
+#define ENA_m1 10        // Enable/speed motor Front 
+#define ENB_m1 3        // Enable/speed motor Back
 
 #define IN_11  9     // L298N #1 in 1 motor Front Left
-#define IN_12  8     // L298N #1 in 2 motor Front left I think one of these must be left
+#define IN_12  8     // L298N #1 in 2 motor back left 
 #define IN_13  5     // L298N #1 in 3 motor Back Right
-#define IN_14  4     // L298N #1 in 4 motor Back Right left?
+#define IN_14  4     // L298N #1 in 4 motor front right
 
-int speedy = 50; // 50 - 255.
-//int speed_Coeff = 4;
-char t;
+int speedy = 50; // 50 - 255 (speed options)
+
+char t; //initializes character for serial reading
 
 
 void setup() {
@@ -19,8 +19,8 @@ void setup() {
   //pinMode(-9,OUTPUT);   //left
   //pinMode(8,OUTPUT);   //right 
 
-  pinMode(ENA_m1, OUTPUT);//speed front right
-  pinMode(ENB_m1, OUTPUT);//speed back right
+  pinMode(ENA_m1, OUTPUT);
+  pinMode(ENB_m1, OUTPUT);
  
     pinMode(IN_11, OUTPUT);
     pinMode(IN_12, OUTPUT);
@@ -33,13 +33,17 @@ Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ //if serial is available, read
   if(Serial.available()>0){
   t = Serial.read();
-  //Serial.println(t);
+
 }
+  //if connection is lost, halt
 if(digitalRead(BTState)==LOW) { state='S'; }
 
+//if speed control is 1, car is slow
+  //if speed control is 2, car is medium speed
+  //if speed control is 3, car is fast
 else if (t == '1'){
       speedy=50;}
     else if (t == '2'){
@@ -47,6 +51,7 @@ else if (t == '1'){
     else if (t == '3'){
       speedy=100;}
 
+ //turns car left
 else if(t == 'l' || t == 'L'){
   digitalWrite(IN_11,HIGH);//front left
   digitalWrite(IN_12, LOW);//back left
@@ -55,9 +60,9 @@ else if(t == 'l' || t == 'L'){
   digitalWrite(IN_13, LOW);//back right
   digitalWrite(IN_14,HIGH);//front right
   analogWrite(ENB_m1, speedy);
-  //Serial.println("moving forward");
+  
 }
-
+//turns car right
 else if(t == 'r' || t == 'R'){
   
   digitalWrite(IN_11,LOW);
@@ -66,18 +71,18 @@ else if(t == 'r' || t == 'R'){
   digitalWrite(IN_13, HIGH);
   digitalWrite(IN_14,LOW);
   analogWrite(ENB_m1, speedy);
-  //Serial.println("moving backward");
+  
 }
-
+//car moves forwards
 else if (t == 'f' || t == 'F'){
   digitalWrite(IN_11,LOW);
   digitalWrite(IN_12,HIGH);
   analogWrite(ENA_m1, speedy);
   digitalWrite(IN_13,LOW);
   digitalWrite(IN_14,HIGH);
- // Serial.println("moving left");
-}
 
+}
+//car stops
 else if (t == 's' || t == 'S'){
   digitalWrite(IN_11,LOW);
   digitalWrite(IN_12,LOW);
@@ -85,10 +90,11 @@ else if (t == 's' || t == 'S'){
   digitalWrite(IN_13,LOW);
   digitalWrite(IN_14,LOW);
   analogWrite(ENB_m1, speedy);
-  //Serial.println("stop");
+
   
   
 }
+  //car moves backwards
 else if (t == 'b' || t == 'B'){
   digitalWrite(IN_11,HIGH);
   digitalWrite(IN_12,LOW);
@@ -96,11 +102,12 @@ else if (t == 'b' || t == 'B'){
   analogWrite(ENA_m1, speedy);
   digitalWrite(IN_14,LOW);
   
-  //Serial.println("moving right");
+
 
 
   
 }
+  //if no input is received, car halts
 else{
   digitalWrite(IN_11,LOW);
   digitalWrite(IN_12,LOW);
